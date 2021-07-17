8e5a0357a653f483f3068c95ae84937e6a2ac7f9
Nicolas Nathan Campos <camposnicolas45@gmail.com>
2021-07-17 15:00:34 -0300

    Testing update OTA

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from "expo-updates";
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        Alert.alert('O aplicativo está atualizando...', '',  [
+          { text: "OK", onPress: async () => {
+            await Updates.reloadAsync(); // depende da sua estratégia
+          } }
+        ]);  
+      }
+    }
+    
+    
+    updateApp();
+  }, []);
+
+  // release de teste - release-channel development
+  // release de producao - default
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app - versao 2.1.0</Text>
+      <Text>MODO PRODUCAO </Text>
+      {/* <Text>MODO DEV </Text> */}
       <StatusBar style="auto" />
     </View>
   );
